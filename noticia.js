cfeee3ae43ad17a4436c5cd1b890eea9f8c3516c
Rafael Vieira <99288619+rafaelvieiradossantos@users.noreply.github.com>
2025-09-25 17:18:30 +0000

    Noticias JS e cartao

--- noticia.js
+++ noticia.js
@@ -15,5 +15,20 @@ function lerNoticias(){
 function carregarNoticias(){
     dados.forEach (noticia => {
         const cartao = document.createElement('div');
+
+        cartao.className = 'cartao';
+
+        cartao.innerHTML = `
+        <img src="${noticia.imagem}"  alta = "${noticia.titulo}" class="imagem">
+        <section>
+            <h2 class="titulo-noticia-cartao">${noticia.titulo}</h2>
+            <p>${noticia.categoria} - ${noticia.data}</p>
+            <p class="descricao-noticia-cartao">${noticia.descricao}</p>
+            <p class="autor-noticia-cartao">${noticia.autor}</p>
+        </section>
+        <a class="botao-cartao" href="./detalhe.html?id=${noticia.id}">Leia Mais</a>
+        `;
+
+        container.appendChild(cartao);
     });
 }
